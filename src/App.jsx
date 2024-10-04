05511a0343221ec565766e96d4f095b88c615f6e
Dowelle Dayle Mon <dowelledaylemon@gmail.com>
2024-10-04 08:03:27 +0000

    Add banner assets for every agents

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Nav from "./components/Nav"
+
+function App() {
+
+
+  return (
+    <div>
+      <Nav />
+      <Router>
+        <Routes>
+
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import styles from '../css/Nav.module.css'
+
+
+const Nav = () => {
+    return(
+        <div>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+export default Nav;
